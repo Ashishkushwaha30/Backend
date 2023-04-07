8064505f91f0f24faa6edc49381e91e6d01e8ee8
ashish <ashishkumarkushwaha007@gmail.com>
2023-04-07 10:10:13 +0530

    changes

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const connectToMongo = require('./db');
 const express = require('express')
+var cors = require('cors') 
 
 connectToMongo();
 const app = express()
-const port = 3000
+const port = 5000
 
-// available routes
-app.use('/api/auth', require('./routes/auth.js'))
-app.use('/api/notes', require('./routes/notes.js'))
+app.use(cors())
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`iNotebook backend listening at http://localhost:${port}`)
 })
