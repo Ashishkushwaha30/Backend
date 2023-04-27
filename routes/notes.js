7dec40eb58a6ac79c6af741ceadbdde6de8dbff1
Arjun2002tiwari <Arjarjun200@gmail.com>
2023-04-28 00:30:41 +0530

    some changes

--- routes/notes.js
+++ routes/notes.js
@@ -1,87 +1,67 @@
+const multer=require("multer");
 const express = require('express');
 const router = express.Router();
-const fetchuser = require('../middleware/fetchuser');
-const Note = require('../models/Notes');
-const { body, validationResult } = require('express-validator');
+const File=require('../models/Files');
+const path = require('path');
 
-// ROUTE 1: Get All the Notes using: GET "/api/notes/getuser". Login required
-router.get('/fetchallnotes', fetchuser, async (req, res) => {
-    try {
-        const notes = await Note.find({ user: req.user.id });
-        res.json(notes)
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Internal Server Error");
-    }
+
+const storage=multer.diskStorage({
+  destination:'./pdf',
+  filename:(req,file,cb)=>{
+      return cb(null,`${file.originalname}`)
+  },
+});
+
+const upload=multer({
+  storage:storage,
 })
 
-// ROUTE 2: Add a new Note using: POST "/api/notes/addnote". Login required
-router.post('/addnote', [
-    body('title', 'Enter a valid title').isLength({ min: 3 }),
-    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
-        try {
-            const { title, description, tag } = req.body;
+router.post('/upload', upload.single('file'), async (req, res) => {
 
-            // If there are errors, return Bad request and the errors
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                return res.status(400).json({ errors: errors.array() });
-            }
-            const note = new Note({
-                title, description, tag, user: req.user.id
-            })
-            const savedNote = await note.save()
+  const { originalname, buffer} = req.file;
+  const extension = path.extname(originalname);
 
-            res.json(savedNote)
+  if (extension !== '.pdf') {
+    return res.status(400).send('Only PDF files are allowed');
+  }
 
-        } catch (error) {
-            console.error(error.message);
-            res.status(500).send("Internal Server Error");
-        }
-    })
+  const file = new File({
+    _id:req.body.id,
+    name: originalname,
+    link:`http://localhost:5000/api/pdf/${originalname}`,
+    chapter:req.body.chapter,
+    time:req.body.time,
+    Bstyle:req.body.Bstyle,
+    title:req.body.title,
+    desc:req.body.desc
+    
+  });
 
-// ROUTE 3: Update an existing Note using: PUT "/api/notes/updatenote". Login required
-router.put('/updatenote/:id', fetchuser, async (req, res) => {
-    const { title, description, tag } = req.body;
+  await file.save();
+
+  res.send('PDF uploaded successfully');
+});
+
+
+router.get('/', async (req, res) => {
+    //const _id = req.params.id;
+  
     try {
-        // Create a newNote object
-        const newNote = {};
-        if (title) { newNote.title = title };
-        if (description) { newNote.description = description };
-        if (tag) { newNote.tag = tag };
+      
+      const pdfs = await File.find({}, '_id name link chapter time Bstyle title desc');
+      //const pdfsName = await File.find({}, 'name');
+      const count = await File.countDocuments();
+  
+      if (!pdfs) {
+        return res.status(404).send('PDF not found');
+      }
+      
+      // res.setHeader('Content-Type', 'application/pdf');
+      res.send({count,pdfs});
 
-        // Find the note to be updated and update it
-        let note = await Note.findById(req.params.id);
-        if (!note) { return res.status(404).send("Not Found") }
-
-        if (note.user.toString() !== req.user.id) {
-            return res.status(401).send("Not Allowed");
-        }
-        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
-        res.json({ note });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Internal Server Error");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
     }
-})
-
-// ROUTE 4: Delete an existing Note using: DELETE "/api/notes/deletenote". Login required
-router.delete('/deletenote/:id', fetchuser, async (req, res) => {
-    try {
-        // Find the note to be delete and delete it
-        let note = await Note.findById(req.params.id);
-        if (!note) { return res.status(404).send("Not Found") }
-
-        // Allow deletion only if user owns this Note
-        if (note.user.toString() !== req.user.id) {
-            return res.status(401).send("Not Allowed");
-        }
-
-        note = await Note.findByIdAndDelete(req.params.id)
-        res.json({ "Success": "Note has been deleted", note: note });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Internal Server Error");
-    }
-})
-module.exports = router
+  });
+module.exports=router;
